52068f694b892d4aa33b210aef8d41d794527fd6
hm-pickl <hm-pickl@hm.edu>
2022-06-03 15:14:22 +0200

    added tapping horse

--- datensammeln/videocapture.py
+++ datensammeln/videocapture.py
@@ -3,16 +3,22 @@
 
 import enum
 import numpy as np
-import keyboard
+import imutils
+#import keyboard
 import cv2
 from datetime import datetime
 from pip import main
+import time
+import threading
+
+curr_num = 0
 
 def main():
+    
     now = datetime.now()
     date_time = now.strftime("%m%d%Y_%H:%M:%S")
     capturestring = ("videos/smart_hans_" + date_time +".avi")
-    duration = 30
+    duration = 15
     framerate = 30.0
     print(capturestring)
     cap = cv2.VideoCapture(0)
@@ -23,15 +29,61 @@ def main():
     height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT )
     print(width)
     print(height)
+
+    num = int(input("Type in number you think of and press Enter to continue...: "))
+
+    capturestring = ("videos/smart_hans_" + date_time + "_target_number_"+ str(num) + ".avi")
+
+    # multithreading:
+    print("Main    : before creating thread")
+    thread_play_tap = threading.Thread(target=playTap, args=(num,))
+    #thread_play_tap.isDaemon()
+    thread_record = threading.Thread(target=recordVideo, args=(cap, capturestring, duration,framerate))
+    print("Main    : before running thread")
+    thread_play_tap.start()
+    thread_record.start()
+    print("Main    : wait for the thread to finish")
+    #thread_play_tap.join()
+    print("Main    : all done")
     
-    input("Press Enter to continue...")
-    recordVideo(cap, capturestring, duration,  framerate)
+    
+    #recordVideo(cap, capturestring, duration,  framerate)
   #  from datetime import datetime
+
+def playTap(num):
+    global curr_num
+    
+
+    
+    for i in range(num):
+        time.sleep(1.2)
+        try:
+            vs = cv2.VideoCapture("HANS_Repo\datensammeln\Horse_Tapping_One_Tap.mp4")
+        except:
+            print("Video file not found")
+        while True:
+            ret, img = vs.read()
+
+            try:
+                #cv2.startWindowThread()
+                img = imutils.resize(img, width=380)
+                
+                cv2.imshow("Horse Tapping", img)
+                cv2.setWindowProperty("Horse Tapping", cv2.WND_PROP_TOPMOST, 1)
+                
+                cv2.waitKey(1)
+            except:
+                break
+        curr_num = i + 1
+        
+    vs.release()
+    #cv2.destroyAllWindows();
+
 
     
 
 def recordVideo(cap, capturestring, duration, framerate):
-
+    
     #Define amount of Frames to Record
     frames_to_record = int(duration*framerate)
 
@@ -55,7 +107,9 @@ def recordVideo(cap, capturestring, duration, framerate):
         # write the flipped frame
         frameNP[i]=frame
         cv2.putText(frame, str(i), [300,100], cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 3)
+        cv2.putText(frame, "current number: " + str(curr_num), [400,100], cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 3)
         cv2.imshow('frame', frame)
+        cv2.setWindowProperty("frame", cv2.WND_PROP_TOPMOST, 2)
         
         if cv2.waitKey(1) == ord('q'):
             break
