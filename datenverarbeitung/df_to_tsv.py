e01c778c26a8287829239cbc3dec225c4b0b3990
hm-pickl <hm-pickl@hm.edu>
2022-08-30 10:21:40 +0200

    added scenario2

--- datenverarbeitung/df_to_tsv.py
+++ datenverarbeitung/df_to_tsv.py
@@ -1,8 +1,9 @@
 
 from scenario1 import get_scenario_1
+from scenario2 import get_scenario_2
 
 
-train, test = get_scenario_1(path = "C:/Users/peter/Nextcloud/smart_hans/AP2/Daten/gesammelt")
+train, test = get_scenario_2(path = "C:/Users/peter/Nextcloud/smart_hans/AP2/Daten/gesammelt")
 
 
 print(test)
@@ -10,5 +11,5 @@ print(train)
 
 
 
-test.to_csv('example_test.tsv', sep="\t", index= False, header= False)
-train.to_csv('example_train.tsv', sep="\t", index= False, header= False)
+test.to_csv('C:/Users/peter/Documents/Projekte/HANS/matlab/example_test.tsv', sep="\t", index= False, header= False)
+train.to_csv('C:/Users/peter/Documents/Projekte/HANS/matlab/example_train.tsv', sep="\t", index= False, header= False)
