ce36e42ea71f04519a45130b382794b5ead8e86c
djbob0 <peterfunny87@gmail.com>
2022-09-02 14:01:23 +0200

    modularised plots

--- Machine_learning/vis/vis_data.py
+++ Machine_learning/vis/vis_data.py
@@ -2,10 +2,10 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import random
 
-def visualize(df):
-    x,y = df.shape
+def visualize(df, fname, show):
+    x = df.shape[0]
     plots_per_sub = int(x / 12)
-    colors = ['r', 'g', 'b']
+    colors = ['r', 'g', 'b', 'c']
     for i in range(12):
         i = i + 1
         for j in range(plots_per_sub):
@@ -14,7 +14,11 @@ def visualize(df):
             row = j*(i)
 
             target = int(df.iloc[row, 0].item())
-            temp_df = df.iloc[row, 1:]
-            temp_df.plot(ax=ax, color = colors[target])
-    plt.show()
+            temp_df = df.iloc[row, 1:-1]
+            temp_df.plot(ax=ax, color = colors[target], sharex = True, sharey = True, figsize = (10,8))
+
+    if show:
+        plt.show()
+    else:
+        plt.savefig(fname= fname, format = 'PNG', dpi = 'figure')
     return
--- Machine_learning/vis_test.py
+++ Machine_learning/vis_test.py
@@ -1,6 +1,9 @@
-from datenverarbeitung import scenario3
+from datenverarbeitung.scenario3 import dataloader
 from vis import vis_data
 
 
-train, test = scenario3.get_scenario_3(path = "C:/Users/peter/Nextcloud/smart_hans/AP2/Daten/gesammelt", nr_taps= 1, move_window_by=-10)
-vis_data.visualize(train)
+dl = dataloader(scenario= 2, path="C:/Users/peter/Nextcloud/smart_hans/AP2/Daten/gesammelt", nr_taps=1, move_window_by=-10)
+train, test, df_labled = dl.get_train_test(frac = 0.8, seed = 0)
+
+print(df_labled)
+vis_data.visualize(df = df_labled, fname = 'vis/plots/scenario3.png', show = True)
