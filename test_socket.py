f8be2cef6803e53cd2d1391e338b44f729acfd92
djbob0 <peterfunny87@gmail.com>
2023-04-25 13:45:39 +0200

    refractored first tests

--- test_socket.py
+++ test_socket.py
@@ -1,25 +1,10 @@
 import socket
+from utils.helper import *
 
 # Define the IP address and port of the Raspberry Pi
 IP_ADDRESS = '192.168.1.195'  # Replace with the IP address of your Raspberry Pi
 PORT = 1234
 
-# Create a socket object
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s = connect_socket(IP_ADDRESS, PORT)
 
-# Connect to the Raspberry Pi
-s.connect((IP_ADDRESS, PORT))
-
-# Send a number to the Raspberry Pi
-number = 42
-s.sendall(str(number).encode())
-
-# Receive the response from the Raspberry Pi
-response = s.recv(1024).decode()
-result = response
-
-# Close the socket
-s.close()
-
-# Print the result
-print(f"The boolean value is {result}")
+feedback = get_feedback(s, 20)
